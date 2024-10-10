32311fdad22ebd0388248055a402bcadd108e363
marc j. greenberg <marc@buildinglink.com>
2024-10-10 00:57:26 -0400

    update start mechanism

--- start.sh
+++ start.sh
@@ -1,11 +1,27 @@
-if [ "$1" == "dev" ]; then
+# This script is used to manage different build and development tasks for the project.
+# Usage: ./start.sh [build|dev|server]
+
+# Check if the first argument is "build"
+if [ "$1" == "build" ]; then
+  # Run the build process using yarn
+  yarn build
+
+# Check if the first argument is "dev"
+elif [ "$1" == "dev" ]; then
+  # If the .env.local file does not exist, generate it using the smash command
   if [ ! -f ./.env.local ]; then
     node --no-warnings ./smash -k motd ./.env.local
   fi
-  node --no-warnings --env-file=./.env.local ./motd.js server
+  # Start the development server using yarn
+  yarn dev
+
+# If the first argument is neither "build" nor "dev"
 else
+  # If the .env file does not exist, generate it using the smash command
   if [ ! -f ./.env ]; then
     node --no-warnings ./smash -k motd ./.env
   fi
-  node --no-warnings --env-file=./.env ./motd.js server
+  # Start the server using yarn
+  yarn server
+
 fi
